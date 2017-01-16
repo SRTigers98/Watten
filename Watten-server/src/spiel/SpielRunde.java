7fe3b6f547aa35b0bdfafa26f94305f489fcd733
Benjamin <ben-eder@freenet.de>
2017-01-16 15:05:23 +0100

    SpielRunde

--- Watten-server/src/spiel/SpielRunde.java
+++ Watten-server/src/spiel/SpielRunde.java
@@ -1,5 +1,43 @@
 package spiel;
 
+import karten.Karte;
+
 public class SpielRunde {
+
+	private static final int SPIELER_1 = 1;
+	private static final int SPIELER_2 = 2;
+
+	private Karte sp1;
+	private Karte sp2;
+	private int sieger;
+
+	public SpielRunde(Karte sp1, Karte sp2) {
+		this.sp1 = sp1;
+		this.sp2 = sp2;
+	}
+
+	public Karte getSp1() {
+		return sp1;
+	}
+
+	public void setSp1(Karte sp1) {
+		this.sp1 = sp1;
+	}
+
+	public Karte getSp2() {
+		return sp2;
+	}
+
+	public void setSp2(Karte sp2) {
+		this.sp2 = sp2;
+	}
+
+	public int getSieger() {
+		return sieger;
+	}
+
+	public void setSieger(int sieger) {
+		this.sieger = sieger;
+	}
 
 }
