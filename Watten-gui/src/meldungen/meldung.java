2c31c62d62519670a79edc1d1917cff3f063ce8b
Marc <goldddstar@googlemail.com>
2017-01-16 15:39:42 +0100

    Default Button Hauptmenue meldung oberklasse hinzugefügt

---
+++ Watten-gui/src/meldungen/meldung.java
@@ -0,0 +1,48 @@
+package meldungen;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+public class meldung {
+
+	private JFrame frame;
+	private JPanel lower_panel;
+	private JPanel upper_panel;
+
+	/**
+	 * Create the application.
+	 */
+	public meldung() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 450);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(getLower_panel(), BorderLayout.SOUTH);
+		frame.getContentPane().add(getUpper_panel(), BorderLayout.CENTER);
+		frame.setVisible(true);
+	}
+
+	private JPanel getLower_panel() {
+		if (lower_panel == null) {
+			lower_panel = new JPanel();
+		}
+		return lower_panel;
+	}
+
+	private JPanel getUpper_panel() {
+		if (upper_panel == null) {
+			upper_panel = new JPanel();
+		}
+		return upper_panel;
+	}
+}
