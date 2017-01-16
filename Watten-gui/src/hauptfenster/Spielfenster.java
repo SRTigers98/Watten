2191ce8a46e39839c610fe72c9f95bbc064be88b
Marc <goldddstar@googlemail.com>
2017-01-16 14:41:35 +0100

    Neu Watten-gui

---
+++ Watten-gui/src/hauptfenster/Spielfenster.java
@@ -0,0 +1,46 @@
+package hauptfenster;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JEditorPane;
+import java.awt.GridBagConstraints;
+
+public class Spielfenster {
+
+	private JFrame frame;
+
+	/**
+	 * Create the application.
+	 * 
+	 * @param string
+	 *            Der Spielername wird übergeben.
+	 */
+	public Spielfenster(String name) {
+		initialize();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.NORTH);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[] { 0, 0 };
+		gbl_panel.rowHeights = new int[] { 0, 0 };
+		gbl_panel.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
+		panel.setLayout(gbl_panel);
+	}
+
+}
