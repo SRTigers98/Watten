3b84c672579020dd15c6bd167392480628a9a311
Benjamin <ben-eder@freenet.de>
2017-01-17 09:43:08 +0100

    Savestate

---
+++ Watten-gui/src/de/verbund/watten/client/ClientImpl.java
@@ -0,0 +1,16 @@
+package de.verbund.watten.client;
+
+public class ClientImpl implements Client {
+
+	private WattenClient client;
+
+	public ClientImpl(WattenClient client) {
+		this.client = client;
+	}
+
+	@Override
+	public void sendeName(String name) {
+
+	}
+
+}
