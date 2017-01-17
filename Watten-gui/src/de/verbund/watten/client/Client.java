c5b4e5e4da5891181843c7716489805f56a6846b
Benjamin <ben-eder@freenet.de>
2017-01-17 09:57:40 +0100

    JavaDoc

--- Watten-gui/src/de/verbund/watten/client/Client.java
+++ Watten-gui/src/de/verbund/watten/client/Client.java
@@ -2,6 +2,12 @@ package de.verbund.watten.client;
 
 public interface Client {
 
+	/**
+	 * Sendet den Namen des Spielers an den Server
+	 * 
+	 * @param name
+	 *            der zu übermittelnde Name des Spielers
+	 */
 	public void sendeName(String name);
 
 }
--- Watten-gui/src/de/verbund/watten/client/gui/ClientGUI.java
+++ Watten-gui/src/de/verbund/watten/client/gui/ClientGUI.java
@@ -2,6 +2,11 @@ package de.verbund.watten.client.gui;
 
 public interface ClientGUI {
 
+	/**
+	 * Gibt die empfangene Nahchricht dem Benutzer aus
+	 * 
+	 * @param message
+	 */
 	public void ausgabe(String message);
 
 }
