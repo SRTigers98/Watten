af8d1fac915059ba18ca4650a717c09c3f71bd76
Marc <goldddstar@googlemail.com>
2017-01-18 10:33:20 +0100

    Update Auswahlfenster Konstanten

--- Watten-common/src/konstanten/AuswahlfensterKonst.java
+++ Watten-common/src/konstanten/AuswahlfensterKonst.java
@@ -10,10 +10,6 @@ package konstanten;
 public class AuswahlfensterKonst {
 
 	// Meldung:
-	public static final int EICHEL = 6;
-	public static final int HERZ = 5;
-	public static final int SCHELLN = 4;
-	public static final int BLAU = 3;
 	public static final int FARBE_WAHL = 2;
 	public static final int SCHLAG_WAHL = 1;
 
