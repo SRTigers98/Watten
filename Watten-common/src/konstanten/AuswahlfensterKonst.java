09c63991894fa7b99d1b5dc8cd183724b6372d36
Marc <goldddstar@googlemail.com>
2017-01-18 10:32:41 +0100

    Konstanten für Auswahlfenster

---
+++ Watten-common/src/konstanten/AuswahlfensterKonst.java
@@ -0,0 +1,20 @@
+package konstanten;
+
+/**
+ * 
+ * Enthält die Konstanten zur Meldungs-Klasse.
+ * 
+ * @author Benjamin
+ *
+ */
+public class AuswahlfensterKonst {
+
+	// Meldung:
+	public static final int EICHEL = 6;
+	public static final int HERZ = 5;
+	public static final int SCHELLN = 4;
+	public static final int BLAU = 3;
+	public static final int FARBE_WAHL = 2;
+	public static final int SCHLAG_WAHL = 1;
+
+}
