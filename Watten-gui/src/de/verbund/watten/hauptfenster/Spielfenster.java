118f588e4238668f28f084f2690861c234fbebbd
Sven <svenderheld@gmail.com>
2017-01-18 13:53:49 +0100

    Ansage einlesen

--- Watten-gui/src/de/verbund/watten/hauptfenster/Spielfenster.java
+++ Watten-gui/src/de/verbund/watten/hauptfenster/Spielfenster.java
@@ -88,6 +88,7 @@ public class Spielfenster implements ClientGUI {
 	private JLabel lblFarbe;
 	private JLabel lblSchlagWert;
 	private JLabel lblFarbeWert;
+	private JLabel lblAnsage;
 
 	/**
 	 * Create the application.
@@ -646,29 +647,34 @@ public class Spielfenster implements ClientGUI {
 			}
 			GridBagLayout gbl_panelAnsage = new GridBagLayout();
 			gbl_panelAnsage.columnWidths = new int[]{0, 0, 0};
-			gbl_panelAnsage.rowHeights = new int[]{0, 0, 0, 0, 0};
+			gbl_panelAnsage.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
 			gbl_panelAnsage.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-			gbl_panelAnsage.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+			gbl_panelAnsage.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 			panelAnsage.setLayout(gbl_panelAnsage);
+			GridBagConstraints gbc_lblAnsage = new GridBagConstraints();
+			gbc_lblAnsage.insets = new Insets(0, 0, 5, 5);
+			gbc_lblAnsage.gridx = 0;
+			gbc_lblAnsage.gridy = 0;
+			panelAnsage.add(getLblAnsage(), gbc_lblAnsage);
 			GridBagConstraints gbc_lblSchlag = new GridBagConstraints();
-			gbc_lblSchlag.insets = new Insets(0, 0, 5, 0);
+			gbc_lblSchlag.insets = new Insets(0, 0, 5, 5);
 			gbc_lblSchlag.gridx = 0;
-			gbc_lblSchlag.gridy = 0;
+			gbc_lblSchlag.gridy = 1;
 			panelAnsage.add(getLblSchlag(), gbc_lblSchlag);
 			GridBagConstraints gbc_lblFarbe = new GridBagConstraints();
-			gbc_lblFarbe.insets = new Insets(0, 0, 5, 0);
+			gbc_lblFarbe.insets = new Insets(0, 0, 5, 5);
 			gbc_lblFarbe.gridx = 0;
-			gbc_lblFarbe.gridy = 1;
+			gbc_lblFarbe.gridy = 2;
 			panelAnsage.add(getLblFarbe(), gbc_lblFarbe);
 			GridBagConstraints gbc_lblSchlagWert = new GridBagConstraints();
 			gbc_lblSchlagWert.insets = new Insets(0, 5, 5, 0);
 			gbc_lblSchlagWert.gridx = 1;
-			gbc_lblSchlagWert.gridy = 0;
+			gbc_lblSchlagWert.gridy = 1;
 			panelAnsage.add(getLblSchlagWert(), gbc_lblSchlagWert);
 			GridBagConstraints gbc_lblFarbeWert = new GridBagConstraints();
 			gbc_lblFarbeWert.insets = new Insets(0, 5, 5, 0);
 			gbc_lblFarbeWert.gridx = 1;
-			gbc_lblFarbeWert.gridy = 1;
+			gbc_lblFarbeWert.gridy = 2;
 			panelAnsage.add(getLblFarbeWert(), gbc_lblFarbeWert);
 		}
 		return panelAnsage;
@@ -792,4 +798,10 @@ public class Spielfenster implements ClientGUI {
 	public void gibFarbe(String farbe) {
 		lblFarbeWert.setText(farbe);
 		}
+	private JLabel getLblAnsage() {
+		if (lblAnsage == null) {
+			lblAnsage = new JLabel("Ansage:");
+		}
+		return lblAnsage;
+	}
 }
