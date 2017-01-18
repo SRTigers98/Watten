86838996ca3e946be3d20f0f0b498292a5d54852
Benjamin <ben-eder@freenet.de>
2017-01-18 09:13:23 +0100

    Konstanten

---
+++ Watten-common/src/konstanten/Konstanten.java
@@ -0,0 +1,13 @@
+package konstanten;
+
+@SuppressWarnings("unused")
+public class Konstanten {
+
+	// Meldung:
+	private static final int HINWEIS = 3;
+	private static final int FEHLER = 2;
+	private static final int WARNUNG = 1;
+
+	// Kommandos:
+
+}
